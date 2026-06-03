609bc02f568e28ae60e868d7d3ac79ef0c01f736
Joaquin Araujo <joaquin.araujo@abstracta.com.uy>
2026-06-03 16:11:06 -0300

    Update copyright declaration aligned with Apache 2.0

--- src/test/java/com/comcast/viper/hlsparserj/MediaPlaylistPdtTest.java
+++ src/test/java/com/comcast/viper/hlsparserj/MediaPlaylistPdtTest.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2015 Comcast Cable Communications Management, LLC
+ * Copyright 2025 Perforce Software, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
